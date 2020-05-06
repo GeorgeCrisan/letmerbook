1648d8a59ba8a300629662dcea44dd644d018f16
George Crisan <georgerdp@gmail.com>
2020-05-06 22:30:34 +0100

    chore: add Github actions deploy.yml

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+//import dotenv from 'dotenv';
+//dotenv.config();
+//require('dotenv').config()
 
 function App() {
+  let apikey = process.env.REACT_APP_API_KEY;
+  console.log(apikey, 'key is this', process.cwd(), process.env.NODE_ENV);
   return (
     <div className="App">
       Hi
