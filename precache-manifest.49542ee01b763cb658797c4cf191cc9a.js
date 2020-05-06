25adcfa0f1a353ead3283a26102bcc7192b40b40
GeorgeCrisan <GeorgeCrisan@users.noreply.github.com>
2020-05-06 21:37:16 +0000

    deploy: 9edd5ff5625349b95b8e95bfb9f06a0efb756237

---
+++ precache-manifest.49542ee01b763cb658797c4cf191cc9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70751516696bd4fb004d087f180c03fb",
+    "url": "/letmerbook/index.html"
+  },
+  {
+    "revision": "caefdfa6ebc223c9869c",
+    "url": "/letmerbook/static/js/2.302377f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/letmerbook/static/js/2.302377f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63129cdb591bbd524c5",
+    "url": "/letmerbook/static/js/main.da31155a.chunk.js"
+  },
+  {
+    "revision": "779913f386c06011eeca",
+    "url": "/letmerbook/static/js/runtime-main.f93da9b4.js"
+  }
+]);
